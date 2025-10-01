bb00b8539b11551286c50c50c1639b714f47d304
najmul57 <najmulcse247@gmail.com>
2025-10-01 07:53:28 +0600

    dfd

--- assets/js/main.js
+++ assets/js/main.js
@@ -384,3 +384,15 @@ if (typeof gsap !== 'undefined') {
 
 
 //scroll
+
+// scrollup
+$.scrollUp({
+  scrollName: 'scrollUp', // Element ID
+  topDistance: '300', // Distance from top before showing element (px)
+  topSpeed: 300, // Speed back to top (ms)
+  animation: 'fade', // Fade, slide, none
+  animationInSpeed: 200, // Animation in speed (ms)
+  animationOutSpeed: 200, // Animation out speed (ms)
+  scrollText: '<i class="fab fa-facebook-f"></i>', // Text for element
+  activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+});
